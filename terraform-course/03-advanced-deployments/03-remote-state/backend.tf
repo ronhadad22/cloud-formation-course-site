6e825e9ede54f24840a47a3fab703cd2ff9ebe60
ronbhadad22 <https://www.google.com/support/accounts/bin/answer.py?answer=181692>
2026-03-04 02:39:24 +0200

    Clean up cicd-lab branch - keep only cicd-lab and codecommit-lab directories

--- terraform-course/03-advanced-deployments/03-remote-state/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "int-course-terraform-bucket-class"
-    key            = "advanced-deployments/terraform.tfstate"
-    region         = "us-east-2"
-    encrypt        = true
-  }
-}
